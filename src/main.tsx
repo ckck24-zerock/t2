2cfa8084dcbc7fbcf1642ce6482297ff8fc98c7c
zerock <kangckck24@gmail.com>
2025-02-27 16:09:23 +0900

    init

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+//npm install -D tailwindcss postcss autoprefixer
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
