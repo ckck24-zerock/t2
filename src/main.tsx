928ca938c151fe5e0d16bf8bbe71d9a1c01e99dc
zerock <kangckck24@gmail.com>
2025-02-27 16:10:04 +0900

    init

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 //npm install -D tailwindcss postcss autoprefixer
-
+//npx tailwind init -p
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
