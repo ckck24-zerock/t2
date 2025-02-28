1943e257dc33b4fea3e7b4c6781bbd647210c7ac
zerock <kangckck24@gmail.com>
2025-02-28 09:36:55 +0900

    1111

---
+++ src/components/countContainer.tsx
@@ -0,0 +1,10 @@
+
+function CountContainer() {
+    return (
+        <div className={'bg-amber-200 h-1/3 text-center pt-4 text-4xl bold'}>
+            COUNT CONTAINER
+        </div>
+    );
+}
+
+export default CountContainer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -6,7 +5,5 @@ import App from './App.tsx'
 //npm install -D tailwindcss postcss autoprefixer
 //npx tailwind init -p
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
