928db65d0d84fc576b7e9d4e0def41b5f79348d9
zerock <kangckck24@gmail.com>
2025-02-28 13:42:17 +0900

    1111

--- src/components/kiosk/menuComponent.tsx
+++ src/components/kiosk/menuComponent.tsx
@@ -8,17 +8,21 @@ interface MenuComponentProps {
 function MenuComponent( {menus} : MenuComponentProps) {
 
     const menuLis = menus.map( menu =>
-        <li key={menu.mno}>
-            {menu.name} -- {menu.price}
-        </li>
+        <div key={menu.mno} className="max-w-xs bg-white rounded-2xl shadow-lg overflow-hidden transform transition duration-300 hover:scale-105">
+            <img src={menu.imgName} alt={menu.name} className="w-full h-48 object-cover" />
+            <div className="p-4 text-center">
+                <h2 className="text-lg font-semibold text-gray-800">{menu.name}</h2>
+                <p className="text-gray-600 mt-1 text-lg font-bold">${menu.price}</p>
+            </div>
+        </div>
     )
 
     return (
         <div>
             <div>Menus</div>
-            <ul>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
                 {menuLis}
-            </ul>
+            </div>
         </div>
     );
 }
