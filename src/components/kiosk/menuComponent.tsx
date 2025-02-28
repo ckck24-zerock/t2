a11214ad321ca31d5a3cdf8922182f53fcf818c1
zerock <kangckck24@gmail.com>
2025-02-28 13:45:05 +0900

    1111

--- src/components/kiosk/menuComponent.tsx
+++ src/components/kiosk/menuComponent.tsx
@@ -12,7 +12,7 @@ function MenuComponent( {menus} : MenuComponentProps) {
             <img src={menu.imgName} alt={menu.name} className="w-full h-48 object-cover" />
             <div className="p-4 text-center">
                 <h2 className="text-lg font-semibold text-gray-800">{menu.name}</h2>
-                <p className="text-gray-600 mt-1 text-lg font-bold">${menu.price}</p>
+                <p className="text-gray-600 mt-1 text-lg font-bold">{menu.price}</p>
             </div>
         </div>
     )
@@ -20,6 +20,11 @@ function MenuComponent( {menus} : MenuComponentProps) {
     return (
         <div>
             <div>Menus</div>
+            <div className={'flex m-4 p-2'}>
+                <div className={'m-2 p-2 bg-amber-500'}>Coffee</div>
+                <div className={'m-2 p-2 bg-amber-500'}>Deserts</div>
+                <div className={'m-2 p-2 bg-amber-500'}>Goods</div>
+            </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
                 {menuLis}
             </div>
